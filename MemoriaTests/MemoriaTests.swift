e0b3e3ff169c46f8232015a299aa301ce4b24ad6
mor80 <egmor80@gmail.com>
2024-12-10 19:34:41 +0300

    Initial Commit

---
+++ MemoriaTests/MemoriaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriaTests.swift
+//  MemoriaTests
+//
+//  Created by Emre Guvench on 10.12.2024.
+//
+
+import Testing
+@testable import Memoria
+
+struct MemoriaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
